fbc2d4b76542c8c5a31f134369a335db9ca68393
Andrew Nesbitt <andrewnez@gmail.com>
2020-12-31 12:46:48 +0000

    download packages via IPFS rather than http when seeding

--- seed.js
+++ seed.js
@@ -4,8 +4,19 @@ const uint8ArrayToString = require('uint8arrays/to-string')
 const receiveMsg = function(msg){
   json = JSON.parse(uint8ArrayToString(msg.data))
   console.log(msg.from, "republished", json.name, "... seeding")
-  // TODO attempt to load (and verify) from ipfs before downloading via http
-  forest.addUrltoIPFS(json.name, json.url)
+
+  parts = json.name.split('@')
+  if (json.name.startsWith('@')) {
+    name = '@'+parts[1]
+    version = parts[2]
+  } else {
+    name = parts[0]
+    version = parts[1]
+  }
+
+  // TODO fallback to http if download from IPFS fails or times out
+  forest.downloadPackageFromIPFS(name, version, json.cid)
+  // forest.addUrltoIPFS(json.name, json.url)
 }
 
 forest.subscribePackageAnnoucements(receiveMsg)
