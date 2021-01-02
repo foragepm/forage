9805156ebc1118acf61e6ec446f1154d35f55a8c
Andrew Nesbitt <andrewnez@gmail.com>
2021-01-02 12:03:29 +0000

    use const instead of var

--- omninet.js
+++ omninet.js
@@ -1,18 +1,18 @@
-var ChangesStream = require('changes-stream');
-var forest = require('./lib/forest');
+const ChangesStream = require('changes-stream');
+const forest = require('./lib/forest');
 
-var changes = new ChangesStream({
+const changes = new ChangesStream({
   db: 'https://replicate.npmjs.com/registry',
   include_docs: true,
   since: 'now'
 });
 
 changes.on('data', function (change) {
-  var name = change.doc.name
+  const name = change.doc.name
   if(name){
-    var latest = Object.entries(change.doc.time).filter(([key, value]) => ["created", "modified"].indexOf(key) < 0).sort(function(a, b) { return new Date(b[1]) - new Date(a[1]) })[0]
-    var versionNumber = latest[0]
-    var version = change.doc.versions[versionNumber]
+    const latest = Object.entries(change.doc.time).filter(([key, value]) => ["created", "modified"].indexOf(key) < 0).sort(function(a, b) { return new Date(b[1]) - new Date(a[1]) })[0]
+    const versionNumber = latest[0]
+    const version = change.doc.versions[versionNumber]
     console.log('New release:',name, versionNumber)
     forest.addUrltoIPFS(name+'@'+versionNumber, version.dist.tarball)
   }
