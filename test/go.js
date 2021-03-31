d499e878b8268c0d6f4f6e063b773d6c27f2f6b1
Andrew Nesbitt <andrewnez@gmail.com>
2021-03-31 18:29:12 +0100

    when generating zip files from go modules, results should be repeatable and reproducible

--- test/go.js
+++ test/go.js
@@ -16,8 +16,10 @@ describe('importPackage', async function() {
     version = 'v1.6.1'
     url = "https://proxy.golang.org/github.com/stretchr/testify/@v/v1.6.1.zip"
 
-    var cid = await forest.go.importPackage(db, manager, name, version, url)
+    var pkg = await forest.go.importPackage(db, manager, name, version, url)
 
-    assert.equal(cid, 'bafybgqde7kfgk4ub2rcr3nyukuy3q5b35nb4bxwvgwlg42uu7cyqv2ihryzurlwt2ozhyly3auysnl4idwnt5ii3jzgul7vb5756nzxuqg2c6');
+    var cid = pkg.cid.toString()
+
+    assert.equal(cid, 'bafkreia4pesx6qj2mi77eqkfe4pommjukwf3lomgmczwnytqz6xy4gf7ae');
   });
 })
