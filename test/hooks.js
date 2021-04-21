76ba06195f8257f22e0bae88bf6927e1f094378c
Andrew Nesbitt <andrewnez@gmail.com>
2021-04-21 11:15:11 +0100

    tests should start server on a different port

--- test/hooks.js
+++ test/hooks.js
@@ -6,7 +6,7 @@ exports.mochaHooks = {
     db = forest.connectDB('forest-test')
     await forest.connectIPFS(db)
     server = await createServer(db)
-    server.listen(8005)
+    server.listen(8006)
   },
   afterAll: async function(){
     server.close();
