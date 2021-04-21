e3d8211bfe99172c3239b2e74df1e930629dbacf
Andrew Nesbitt <andrewnez@gmail.com>
2021-04-21 15:03:29 +0100

    Change pubsub message format to include an action
    This is to allow for handling of new types of pubsub message in future

---
+++ test/forage.js
@@ -0,0 +1,75 @@
+var assert = require('assert');
+const forage = require('../lib/forage');
+const uint8ArrayFromString = require('uint8arrays/from-string')
+
+describe('seed', async function() {
+  it('should seed a republish pubsub message', async () => {
+
+    var cid = 'bafybgqde7kfgk4ub2rcr3nyukuy3q5b35nb4bxwvgwlg42uu7cyqv2ihryzurlwt2ozhyly3auysnl4idwnt5ii3jzgul7vb5756nzxuqg2c6'
+
+    var json = {
+      action: 'republish',
+      forage: forage.core.forageVersion(),
+      package: {
+        url: "https://registry.npmjs.org/7zip-bin/-/7zip-bin-5.0.3.tgz",
+        manager: 'npm',
+        name: '7zip-bin',
+        version: '5.0.3',
+        cid: cid,
+        size: '100000'
+      }
+    }
+
+    var msg = {
+      from: 'somepeerid',
+      data: uint8ArrayFromString(JSON.stringify(json))
+    }
+
+    var res = await forage.seed(msg)
+    assert.equal(res, cid)
+  })
+
+  it('should skip a non-republish pubsub message', async () => {
+    var json = {
+      action: 'request',
+      forage: forage.core.forageVersion(),
+      package: {
+        url: "https://registry.npmjs.org/7zip-bin/-/7zip-bin-5.0.3.tgz",
+        manager: 'npm',
+        name: '7zip-bin',
+        version: '5.0.3'
+      }
+    }
+
+    var msg = {
+      from: 'somepeerid',
+      data: uint8ArrayFromString(JSON.stringify(json))
+    }
+
+    var res = await forage.seed(msg)
+    assert.equal(res, false)
+  })
+
+  it('should skip a pubsub messages from itself', async () => {
+    var json = {
+      action: 'republish',
+      forage: forage.core.forageVersion(),
+      package: {
+        url: "https://registry.npmjs.org/7zip-bin/-/7zip-bin-5.0.3.tgz",
+        manager: 'npm',
+        name: '7zip-bin',
+        version: '5.0.3',
+        cid: 'bafffy',
+        size: '100000'
+      }
+    }
+
+    var msg = {
+      from: ipfsID.id,
+      data: uint8ArrayFromString(JSON.stringify(json))
+    }
+
+    var res = await forage.seed(msg)
+    assert.equal(res, false)
+  })
+})
