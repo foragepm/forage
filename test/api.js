4d44244423c3c4dea352afdee9dbe60ec6d5d89c
Andrew Nesbitt <andrewnez@gmail.com>
2021-06-03 13:07:54 +0100

    Move http api code into it's own file plus add more server tests

---
+++ test/api.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('http api', async function() {
+  it('responds to index requests', async () => {
+    var path = `/~/api/packages`
+
+    chai.request(server)
+        .get(path)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.be.json;
+          expect(res).to.have.status(200);
+        })
+  })
+
+  it('respond to missing pages with 404', async () => {
+    var path = `/~/api/foobar`
+
+    chai.request(server)
+        .get(path)
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+        })
+  })
+})
---
+++ test/server.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('createServer', async function() {
+  it('responds to index requests', async () => {
+    var path = `/`
+
+    chai.request(server)
+        .get(path)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.be.html;
+          expect(res).to.have.status(200);
+        })
+  })
+
+  it('respond to missing pages with 404', async () => {
+    var path = `/foobar`
+
+    chai.request(server)
+        .get(path)
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+        })
+  })
+})
