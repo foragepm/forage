688253438077f0e22e99b8d60b2683d656e2afea
Andrew Nesbitt <andrewnez@gmail.com>
2021-06-16 11:10:17 +0100

    Fix api tests

--- test/api.js
+++ test/api.js
@@ -6,38 +6,44 @@ let expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('http api', async function() {
-  it('responds to index requests', async () => {
-    var path = `/~/api/packages`
+  describe('package list endpoint', async function() {
+    it('responds to index requests', async () => {
+      var path = `/~/api/packages`
 
-    chai.request(server)
-        .get(path)
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res).to.be.json;
-          expect(res).to.have.status(200);
-        })
+      chai.request(server)
+          .get(path)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.be.json;
+            expect(res).to.have.status(200);
+          })
+    })
   })
 
-  it('responds to package requests', async () => {
-    var path = `/~/api/packages/go/github.com/ipfs/go-ipfs`
+  describe('individiual package endpoint', async function() {
+    it('responds to package requests', async () => {
+      var path = `/~/api/packages/go/github.com/ipfs/go-ipfs`
 
-    chai.request(server)
-        .get(path)
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res).to.be.json;
-          expect(res).to.have.status(200);
-        })
+      chai.request(server)
+          .get(path)
+          .end((err, res) => {
+            console.log(err)
+            expect(err).to.be.null;
+            expect(res).to.be.json;
+            expect(res).to.have.status(200);
+          })
+    })
   })
 
+  describe('404', async function() {
+    it('respond to missing pages with 404', async () => {
+      var path = `/~/api/foobar`
 
-  it('respond to missing pages with 404', async () => {
-    var path = `/~/api/foobar`
-
-    chai.request(server)
-        .get(path)
-        .end((err, res) => {
-          expect(res).to.have.status(404);
-        })
+      chai.request(server)
+          .get(path)
+          .end((err, res) => {
+            expect(res).to.have.status(404);
+          })
+    })
   })
 })
